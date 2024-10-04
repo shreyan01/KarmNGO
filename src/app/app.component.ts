06598bfc228563a9905de5df70f54de7332f3361
shreyan01 <shreyanshukla19@gmail.com>
2024-10-04 20:54:39 +0530

    addeed all components and improved the style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,23 @@ import { RouterOutlet } from '@angular/router';
 import { BeneficiariesComponent } from './component/beneficiaries.component';
 import { DonateNowComponent } from './component/donationbutton.component';
 import { Belt1Component } from './component/belt1.component';
+import { NavbarComponent } from './component/navbar.component';
+import { FooterComponent } from './component/footer.component';
+import { FieldsComponent } from './component/fields.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BeneficiariesComponent, DonateNowComponent, Belt1Component],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    BeneficiariesComponent, 
+    DonateNowComponent, 
+    Belt1Component,
+    NavbarComponent,
+    FooterComponent,
+    FieldsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
