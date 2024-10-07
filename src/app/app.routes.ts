68df87cca1181f90209347f3e70c591ff4832479
shreyan01 <shreyanshukla19@gmail.com>
2024-10-07 20:27:22 +0530

    fixed routing issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'about-us', component: AboutUsComponent },
+  { path: '', component: HomeComponentComponent }
+];
