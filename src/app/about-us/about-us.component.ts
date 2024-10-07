0adc703f1aaa0ba8181838d59f437a06f279fce0
shreyan01 <shreyanshukla19@gmail.com>
2024-10-07 21:50:39 +0530

    added navbar and footer to about us

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../component/navbar.component';
+import { FooterComponent } from '../component/footer.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
