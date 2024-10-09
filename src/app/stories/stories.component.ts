dd0e86408e0d7b57997ed8ea6b4fe09f15920a27
shreyan01 <shreyanshukla19@gmail.com>
2024-10-09 23:48:56 +0530

    wrote the final code for stories component

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../component/navbar.component';
+import { FooterComponent } from '../component/footer.component';
 
 @Component({
   selector: 'app-stories',
-  standalone: true,
-  imports: [],
   templateUrl: './stories.component.html',
-  styleUrl: './stories.component.css'
+  styleUrls: ['./stories.component.css'],
+  standalone: true,
+  imports: [NavbarComponent, FooterComponent]
 })
 export class StoriesComponent {
-
+  closeDialog(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (target.classList.contains('group-hover:opacity-100')) {
+      target.classList.remove('group-hover:opacity-100', 'group-hover:visible');
+      target.classList.add('opacity-0', 'invisible');
+    }
+  }
 }
