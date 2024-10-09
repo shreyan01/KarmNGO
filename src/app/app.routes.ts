7c1f018903afa745be1d15d29c1e9d0d4172aac0
shreyan01 <shreyanshukla19@gmail.com>
2024-10-09 22:57:57 +0530

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 export const routes: Routes = [
   { path: 'about-us', component: AboutUsComponent },
-  { path: '', component: HomeComponentComponent }
+  { path: '', component: HomeComponentComponent },
+  { path: 'contact', component: ContactComponent }
 ];
