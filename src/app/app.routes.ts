ff322b0e37c475fa00efc4cf6242bfa6be19a550
shreyan01 <shreyanshukla19@gmail.com>
2024-10-09 23:48:22 +0530

    added the route for the stories component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
 import { ContactComponent } from './contact/contact.component';
+import { StoriesComponent } from './stories/stories.component';
 
 
 export const routes: Routes = [
   { path: 'about-us', component: AboutUsComponent },
   { path: '', component: HomeComponentComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'stories', component: StoriesComponent }
 ];
