498c513e687214a0d5d0e6675d7cc124b0ac0804
shreyan01 <shreyanshukla19@gmail.com>
2024-10-09 22:58:45 +0530

    refined contact us form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../component/navbar.component';
+import { FooterComponent } from '../component/footer.component';
 
 @Component({
   selector: 'app-contact',
-  standalone: true,
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css'],
+  standalone: true,
+  imports: [FormsModule, CommonModule, NavbarComponent, FooterComponent]
 })
 export class ContactComponent {
+  contactData = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  };
 
+  onSubmit() {
+    // Handle form submission here
+    console.log('Form submitted', this.contactData);
+    // You can add your logic to send the form data to a server here
+  }
 }
