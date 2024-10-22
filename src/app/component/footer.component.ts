a1f4889c339ead16e7730e2cb451320847ef4881
shreyan01 <shreyanshukla19@gmail.com>
2024-10-22 23:34:58 +0530

    added map location

--- src/app/component/footer.component.ts
+++ src/app/component/footer.component.ts
@@ -36,6 +36,12 @@ import { Component } from '@angular/core';
             <p>123 NGO Street, Charity City</p>
             <p>contact&#64;karmngo.org</p>
             <p>+1 (123) 456-7890</p>
+            <div class="mt-4">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14014.5522528391!2d77.33497345!3d28.6207434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce55d1f1f1f1f%3A0x4b0f0f0f0f0f0f0f!2sSector%2062%2C%20Noida%2C%20Uttar%20Pradesh%2C%20India!5e0!3m2!1sen!2sus!4v1616161616161!5m2!1sen!2sus"
+                width="100%" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+              </iframe>
+            </div>
           </div>
         </div>
         <div class="mt-8 text-center">
